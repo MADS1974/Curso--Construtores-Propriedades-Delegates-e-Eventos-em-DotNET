67c124c391b7117ffea7aec02812dddba7ad97a1
MADS <86740964+MADS1974@users.noreply.github.com>
2022-01-14 01:47:32 -0300

    Enviando Curso Construtores, Propriedades, Delegates e eventos em .NET

---
+++ Construtores-Propriedades-Delegates-e-Eventos-em-DotNET/ExemploConstrutores/Models/Pessoa.cs
@@ -0,0 +1,26 @@
+namespace ExemploConstrutores.Models
+{
+    public class Pessoa
+    {
+        private readonly string nome = "Marcio";
+        private readonly string sobrenome;
+
+        public Pessoa()
+        {
+            nome = string.Empty;
+            sobrenome = string.Empty;
+        }
+
+        public Pessoa(string nome, string sobrenome)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            System.Console.WriteLine("Construtor classe pessoa!");
+        }
+
+        public void Apresentar()
+        {
+            System.Console.WriteLine($"Olá, meu nome é: {nome} {sobrenome}");
+        }
+    }
+}
---
+++ Construtores-Propriedades-Delegates-e-Eventos-em-DotNET/README.md
@@ -0,0 +1,6 @@
+**[My LinkedIn](https://www.linkedin.com/in/mads1974/)**
+
+# Curso tipos de construtores (DIO)
+
+**[Especialista: Professor Leonardo Buta LinkedIn](https://www.linkedin.com/in/leonardo-buta/)**
+
